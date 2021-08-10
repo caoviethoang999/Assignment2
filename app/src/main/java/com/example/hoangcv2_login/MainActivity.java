aa109dfc3ad50fa01f6b275cca77becfbb379dec
caoviethoang <caoviethoang99@gmail.com>
2021-08-10 13:29:47 +0700

    nopbai

---
+++ app/src/main/java/com/example/hoangcv2_login/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hoangcv2_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    }
---
+++ app/src/main/java/com/example/hoangcv2_login/ResetPassActivity.java
@@ -0,0 +1,16 @@
+package com.example.hoangcv2_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ResetPassActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pass);
+    }
+}
